08e45dcc5bb1d0856646e109e7cb943d6a6f7242
Francesco Murdaca <fmurdaca@redhat.com>
2021-09-22 17:10:04 +0200

    Update python script for test
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- src/test.py
+++ src/test.py
@@ -26,9 +26,16 @@ import sys
 
 from tensorflow.keras.datasets import mnist as tf_dataset
 
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+
 import numpy as np
 
 _LOGGER = logging.getLogger(__name__)
+
+USE_PYTORCH = bool(int(os.getenv("TUTORIAL_USE_PYTORCH", 0)))
 
 
 def download_test_dataset():
@@ -46,18 +53,6 @@ def download_test_dataset():
 
 def main_test():
     """Run main test to gather metrics for data scientists and AI DevOps Engineers."""
-    dataset = download_test_dataset()
-
-    x_test = dataset["x_test"]
-
-    y_test = dataset["y_test"]
-
-    # Convert to float32.
-    x_test = np.array(x_test, np.float32)
-
-    # Normalize images value from [0, 255] to [0, 1].
-    x_test = x_test / 255.0
-
     addr = os.getenv("DEPLOYED_MODEL_URL", "http://localhost:8080")
     test_url = addr + "/predict"
 
@@ -66,11 +61,48 @@ def main_test():
 
     results = []
 
-    total_tests = len(x_test)
     n = 1
 
-    for img, number in zip(x_test, y_test):
+    if USE_PYTORCH:
+        # Prepare MNIST data.
+        test_data = datasets.MNIST(
+            root = './data/raw/pytorch-mnist-dataset', 
+            train = False, 
+            transform = ToTensor(),
+        )
+
+        loaders = {
+            'test'  : torch.utils.data.DataLoader(test_data, 
+                                                batch_size=1, 
+                                                shuffle=True, 
+                                                num_workers=1),
+        }
+
+        test_data = loaders['test']
+
+        total_tests = int(os.getenv("TUTORIAL_MAX_REQUESTS_TEST", len(test_data)))
+
+    else:
+        dataset = download_test_dataset()
+
+        x_test = dataset["x_test"]
+
+        y_test = dataset["y_test"]
+
+        # Convert to float32.
+        x_test = np.array(x_test, np.float32)
+
+        # Normalize images value from [0, 255] to [0, 1].
+        x_test = x_test / 255.0
+
+        test_data = zip(x_test, y_test)
+
+        total_tests = int(os.getenv("TUTORIAL_MAX_REQUESTS_TEST", len(x_test)))
+
+    for img, label in test_data:
+        print(n)
         _LOGGER.info(f"test number {n}/{total_tests}")
+
         data = json.dumps({"inputs": img.tolist()})
 
         try:
@@ -87,14 +119,17 @@ def main_test():
         latency = json_response["latency"]
         probability = json_response["probability"]
 
-        if int(number) == int(prediction):
-            error = 1
+        if int(label) == int(prediction):
+            answer = 0
         else:
-            error = 0
+            answer = 1
 
-        results.append({"error": error, "latency": latency, "probability": probability})
+        results.append({"error": answer, "latency": latency, "probability": probability})
 
         n += 1
+
+        if n == total_tests:
+            break
 
     report = {
         "average_latency": np.mean([r["latency"] for r in results]),
